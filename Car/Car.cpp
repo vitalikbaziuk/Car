876c752696551cb7746b669bfeb4fcc74460b2eb
Vitalik Baziuk <vitalik.baziuk228@gmail.com>
2020-07-23 18:53:36 +0300

    Added "AddCar" function

--- Car/Car.cpp
+++ Car/Car.cpp
@@ -12,7 +12,9 @@ private:
 	float engineCapacity;
 	int price;
 public:
-	
+	Car() : name("NoName"), graduationYear(0), engineCapacity(0), price(0) {};
+	Car(string name, int graduationYear, float engineCapacity, int price)
+		: name(name), graduationYear(graduationYear), engineCapacity(graduationYear), price(price) {};
 };
 
 class carDealership
@@ -20,11 +22,19 @@ class carDealership
 private:
 	vector <Car> cars;
 public:
+	void AddCar(string name, int graduationYear, float engineCapacity, int price)
+	{
+		Car car(name, graduationYear, engineCapacity, price);
+		cars.push_back(car);
+	}
 };
 
 int main()
 {
 	carDealership car;
+
+	car.AddCar("car", 2005, 3.5, 1000);
+	car.AddCar("car2", 2007, 3.0, 5000);
 
 	return 0;
 }
