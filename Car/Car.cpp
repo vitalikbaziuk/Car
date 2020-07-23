3bcac0402b231cc54a6191e5d474b6c28e49f8ee
Vitalik Baziuk <vitalik.baziuk228@gmail.com>
2020-07-23 18:52:11 +0300

    Added class

---
+++ Car/Car.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Car
+{
+private:
+	string name;
+	int graduationYear;
+	float engineCapacity;
+	int price;
+public:
+	
+};
+
+class carDealership
+{
+private:
+	vector <Car> cars;
+public:
+};
+
+int main()
+{
+	carDealership car;
+
+	return 0;
+}
